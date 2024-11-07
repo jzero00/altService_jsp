b12a4d2ecf140db9c98d39eb481ad7b1b65d41f2
jzero <07mrjzero@gmail.com>
2024-11-07 22:45:28 +0900

    MemberManageVO 수정

--- src/main/java/altService/sys/member/service/impl/MemberManageVO.java
+++ src/main/java/altService/sys/member/service/impl/MemberManageVO.java
@@ -10,5 +10,68 @@ public class MemberManageVO {
 	private String area_no;
 	private String middle_telno;
 	private String end_telno;
-	
+
+	public String getEmplyr_id() {
+		return emplyr_id;
+	}
+
+	public void setEmplyr_id(String emplyr_id) {
+		this.emplyr_id = emplyr_id;
+	}
+
+	public String getEmplyr_nm() {
+		return emplyr_nm;
+	}
+
+	public void setEmplyr_nm(String emplyr_nm) {
+		this.emplyr_nm = emplyr_nm;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPassword_hint() {
+		return password_hint;
+	}
+
+	public void setPassword_hint(String password_hint) {
+		this.password_hint = password_hint;
+	}
+
+	public String getPassword_cnsr() {
+		return password_cnsr;
+	}
+
+	public void setPassword_cnsr(String password_cnsr) {
+		this.password_cnsr = password_cnsr;
+	}
+
+	public String getArea_no() {
+		return area_no;
+	}
+
+	public void setArea_no(String area_no) {
+		this.area_no = area_no;
+	}
+
+	public String getMiddle_telno() {
+		return middle_telno;
+	}
+
+	public void setMiddle_telno(String middle_telno) {
+		this.middle_telno = middle_telno;
+	}
+
+	public String getEnd_telno() {
+		return end_telno;
+	}
+
+	public void setEnd_telno(String end_telno) {
+		this.end_telno = end_telno;
+	}
 }
