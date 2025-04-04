ea92f04f73d99bb83020667a37315b46bbc193c8
jzero <07mrjzero@gmail.com>
2025-04-04 23:35:56 +0900

    excel insert Controller 작성

---
+++ src/main/java/altService/utils/ExcelController.java
@@ -0,0 +1,13 @@
+package altService.utils;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/excel/")
+public class ExcelController {
+
+	@PostMapping("insertMember.do")
+	public 
+}
