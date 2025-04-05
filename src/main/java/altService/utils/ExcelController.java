368abbaa11f20e87bdf341654750dc9e5ddaf421
jzero <07mrjzero@gmail.com>
2025-04-05 22:03:11 +0900

    multipart resolver 추가

--- src/main/java/altService/utils/ExcelController.java
+++ src/main/java/altService/utils/ExcelController.java
@@ -1,13 +1,22 @@
 package altService.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import altService.sys.member.service.MemberService;
 
 @Controller
 @RequestMapping("/excel/")
 public class ExcelController {
+	
+	@Autowired
+	MemberService mService;
 
 	@PostMapping("insertMember.do")
-	public 
+	public void insertMemberExcel(MultipartFile file) {
+		/*엑셀파일 처리 후 중복체크해서 중복 아닐시 insert*/
+	}
 }
